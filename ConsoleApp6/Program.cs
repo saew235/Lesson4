66ee164897c1f63f1c84f03f222afc5d45cf6cb7
saew <saew0163@gmail.com>
2020-11-24 11:43:50 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            double b = Convert.ToDouble(Console.ReadLine());
+            int c = Convert.ToInt32(Console.ReadLine());
+            double D = b * b - 4 * a * c;
+            if (D < 0)
+            {
+                Console.WriteLine("Sorry, D < 0, no decisions(");
+            }
+            else
+                if (D == 0)
+            {
+                double x = (-b / (2 * a));
+                Console.WriteLine("The discriminant is zero. Answer is " + x + ".");
+            }
+            else
+            {
+                double x1 = ((-b - Math.Sqrt(D)) / (2 * a));
+                double x2 = ((-b + Math.Sqrt(D)) / (2 * a));
+                Console.WriteLine("Discriminant is " + D + ". The answers are " + x1 + ", " + x2);
+            }
+        }
+    }
+}
